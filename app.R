effcb5c513f0e821597d6613b150d86566b13e0d
rmgpanw <rmgpanw@ucl.ac.uk>
2025-02-21 13:12:36 +0000

    add alternative view option with one row per variant. Download excel instead of csv

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(purrr)
 library(otargen)
 library(crosstalk)
+library(openxlsx)
 
 # Define UI for application
 ui <- fluidPage(
@@ -19,8 +20,11 @@ ui <- fluidPage(
         placeholder = "rs12913832"
       ),
       actionButton("retrieve", "Retrieve V2G"),
-      downloadButton("downloadData", "Download as CSV"),
-      uiOutput("filterUI")
+      downloadButton("downloadData", "Download as Excel"),
+      uiOutput("filterUI"),
+      radioButtons("outputType", "Select Output Type:",
+                   choices = list("Table" = "table", "Variant per Row" = "variant_row"),
+                   selected = "table")
     ),
     mainPanel(reactableOutput("v2gTable"), textOutput("errorMessage"))
   )
@@ -58,28 +62,48 @@ server <- function(input, output) {
   })
 
   output$v2gTable <- renderReactable({
-    data <- shared_data()
+    data <- v2g_data()
     if (is.null(data)) {
       output$errorMessage <- renderText("No results found for the provided rsids.")
       return(NULL)
     } else {
       output$errorMessage <- renderText("")
-      reactable(
-        data,
-        columns = list(
-          gene.symbol = colDef(name = "Gene Symbol"),
-          variant = colDef(name = "Variant"),
-          overallScore = colDef(name = "Overall Score"),
-          gene.id = colDef(name = "Gene ID")
-        ),
-        searchable = TRUE,
-        resizable = TRUE,
-        filterable = TRUE,
-        showPageSizeOptions = TRUE,
-        paginationType = "jump",
-        defaultPageSize = 10,
-        pageSizeOptions = c(10, 25, 50, 100, 200)
-      )
+      if (input$outputType == "table") {
+        reactable(
+          shared_data(),
+          columns = list(
+            gene.symbol = colDef(name = "Gene Symbol"),
+            variant = colDef(name = "Variant"),
+            overallScore = colDef(name = "Overall Score"),
+            gene.id = colDef(name = "Gene ID")
+          ),
+          searchable = TRUE,
+          resizable = TRUE,
+          filterable = TRUE,
+          showPageSizeOptions = TRUE,
+          paginationType = "jump",
+          defaultPageSize = 10,
+          pageSizeOptions = c(10, 25, 50, 100, 200)
+        )
+      } else {
+        variant_data <- data %>%
+          group_by(variant_input) %>%
+          summarize(genes = paste(gene.symbol[order(-overallScore)], collapse = ", "))
+        reactable(
+          variant_data,
+          columns = list(
+            variant_input = colDef(name = "Variant"),
+            genes = colDef(name = "Genes (sorted by V2G score)")
+          ),
+          searchable = TRUE,
+          resizable = TRUE,
+          filterable = TRUE,
+          showPageSizeOptions = TRUE,
+          paginationType = "jump",
+          defaultPageSize = 10,
+          pageSizeOptions = c(10, 25, 50, 100, 200)
+        )
+      }
     }
   })
 
@@ -101,10 +125,22 @@ server <- function(input, output) {
 
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste("v2g_data-", Sys.Date(), ".csv", sep = "")
+      paste("v2g_data-", Sys.Date(), ".xlsx", sep = "")
     },
     content = function(file) {
-      write.csv(v2g_data(), file, row.names = FALSE)
+      data <- v2g_data()
+      if (!is.null(data)) {
+        variant_data <- data %>%
+          group_by(variant_input) %>%
+          summarize(genes = paste(gene.symbol[order(-overallScore)], collapse = ", "))
+        
+        wb <- createWorkbook()
+        addWorksheet(wb, "Full table")
+        writeData(wb, "Full table", data)
+        addWorksheet(wb, "Variant per row")
+        writeData(wb, "Variant per row", variant_data)
+        saveWorkbook(wb, file, overwrite = TRUE)
+      }
     }
   )
 }
